ddc9b53246d36773a79eb381b1818257d53cd460
Yanis Verbeke <yanis88357@gmail.com>
2025-01-24 23:14:33 +0100

    Duck speeeeeeeeed

---
+++ Assets/Scripts/Duck.cs
@@ -0,0 +1,42 @@
+using TMPro.EditorUtilities;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    [SerializeField] private float _force = 1;
+    private Vector3 _basePos;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _basePos = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (Mathf.Abs(_rigidbody.linearVelocity.z) < 0.2f)
+            {
+                Propulse();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position = _basePos;
+        }
+    }
+
+    private void Propulse()
+    {
+        _rigidbody.AddForce(Vector3.forward * _force, ForceMode.Impulse);
+    }
+
+    public void ChangeSpeed(float factorNormalized)
+    {
+        _rigidbody.AddForce(factorNormalized * _rigidbody.linearVelocity * 1, ForceMode.Acceleration);
+    }
+}
