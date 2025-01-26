e9f2dc8e9383036aad3f70348065bc058a1a8354
Kryode <anthony.guiock@gmail.com>
2025-01-26 10:48:54 +0100

    Added combo and difficulty systems

--- Assets/Scripts/Duck.cs
+++ Assets/Scripts/Duck.cs
@@ -7,22 +7,23 @@ public class Duck : MonoBehaviour
     [SerializeField] private float _propulseForce = 20;
     [SerializeField] private float stainForce = 2;
     [SerializeField] private float _maxSpeed;
-    [SerializeField] private float maxSpeedSpinIncrease;
-    [SerializeField] private float stainForceSpinIncrease;
+    [SerializeField] private float maxSpeedDifficultyStep;
+    [SerializeField] private float stainForceDifficultyStep;
     [SerializeField] private GameObject _baseModel;
     [SerializeField] private GameObject _bonusModel;
     private bool isAlive = true;
     private bool haveStarted = false;
     private bool _startPressed = false;
-    // Pas debug en fait, on en a vraiment besoin 
-    private Vector3 _basePos;
-
+    [SerializeField] private float deadzone;
+    private Vector3 previousPos;
+    private float scoreUpdateTimer = 0.3f;
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
-        _basePos = transform.position;
+        previousPos = transform.position;
 
+        //Le jeu casse completement sans ce code, on sait pas pourquoi
         if (Random.Range(0, 20) == 0)
         {
             _baseModel.SetActive(false);
@@ -37,16 +38,15 @@ public class Duck : MonoBehaviour
 
     private void Update()
     {
-
-        //Debug code, reset la position du canard a l'origine
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            transform.position = _basePos;
-        }
-
         if (Input.GetButtonDown("Jump") && !haveStarted)
         {
             _startPressed = true;
+        }
+        scoreUpdateTimer -= Time.deltaTime;
+        if(scoreUpdateTimer <= 0)
+        {
+            Calculatescore(previousPos.z);
+            scoreUpdateTimer = 0.3f;
         }
     }
 
@@ -57,7 +57,6 @@ public class Duck : MonoBehaviour
             isAlive = false;
             _rigidbody.linearVelocity = Vector3.zero;
             UiManager.Instance.SetEndScreenVisibility(true);
-            Debug.Log("DEAD!");
         }
         if (_startPressed)
         {
@@ -74,12 +73,7 @@ public class Duck : MonoBehaviour
         {
             _rigidbody.linearVelocity = new Vector3(_rigidbody.linearVelocity.x, _rigidbody.linearVelocity.y, _maxSpeed);
         }
-        //Debug code, reset la position du canard a l'origine
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            transform.position = _basePos;
-        }
-        UiManager.Instance.UpdateScoreDistance((int)(transform.position.z - _basePos.z));
+        
     }
 
     private void Propulse()
@@ -101,8 +95,19 @@ public class Duck : MonoBehaviour
     public void Spin()
     {
         _animator.SetTrigger("Spin");
-        _maxSpeed += maxSpeedSpinIncrease;
-        stainForce += stainForceSpinIncrease;
         EffectsManager.Instance.SpawnSparkles(transform.position + Vector3.forward * 2f);
+        LevelManager.Instance.AddCombo();
+    }
+
+    private void Calculatescore(float previousPos)
+    {
+        LevelManager.Instance.AddScore((int)Mathf.Floor(transform.position.z - previousPos));
+        this.previousPos = transform.position;
+    }
+
+    public void DifficultyIncrease()
+    {
+        _maxSpeed += maxSpeedDifficultyStep;
+        stainForce += stainForceDifficultyStep;
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,16 +2,36 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-
+    public static LevelManager Instance {get; private set;}
     [SerializeField] private GameObject _floor1;
     [SerializeField] private GameObject _floor2;
     [SerializeField] private Duck _duck;
     [SerializeField] private GameObject _stainPrefab;
     [SerializeField] private float distanceOfSpawn;
-
+    private int score = 0;
+    private float comboMultiplier = 1f;
+    [SerializeField] private float comboDuration;
+    [SerializeField] private float comboStep;
+    [SerializeField] private int difficultyStep;
+    public int difficulty;
+    private float timer;
     private float _floorOffset = 64.6f;
 
     private float _nextSpawnPosition;
+
+    private void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        timer = comboDuration;
+    }
 
     private void Update()
     {
@@ -28,6 +48,15 @@ public class LevelManager : MonoBehaviour
         {
             SpawnStain();
         }
+        UiManager.Instance.UpdateScoreDistance(score);
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            comboMultiplier = 1f;
+            timer = comboDuration;
+            Debug.Log("Combo perdu !");
+        }
+
     }
 
     private void SpawnStain()
@@ -35,6 +64,23 @@ public class LevelManager : MonoBehaviour
         Vector3 position = new Vector3(_duck.transform.position.x + Random.Range(-3f, 3f),0 , _duck.transform.position.z + distanceOfSpawn);
         Instantiate(_stainPrefab, position, Quaternion.identity);
         _nextSpawnPosition = _duck.transform.position.z + Random.Range(10f, 25f);
+    }
+
+    public void AddScore(int scoreToAdd)
+    {
+        score += (int)Mathf.Floor(scoreToAdd * comboMultiplier);
+        if(difficulty != score / difficultyStep)
+        {
+            GameObject.Find("DuckHolder").SendMessage("DifficultyIncrease");
+            difficulty++;
+        }
+    }
+
+    public void AddCombo()
+    {
+        comboMultiplier += comboStep;
+        timer = comboDuration;
+        Debug.Log("Combo ! :" + comboMultiplier);
     }
 
 }
