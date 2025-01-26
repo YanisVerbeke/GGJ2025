ba2e410e289c383bfecce7a1439c0b766827f980
unknown <antonin.pulby@edu.itescia.fr>
2025-01-26 03:56:43 +0100

    fix menu

--- Assets/Scripts/Ui/RotateImage.cs
+++ Assets/Scripts/Ui/RotateImage.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 
 public class RotateImage : MonoBehaviour
 {
+    [SerializeField] private float _maxRotate= 20;
     [SerializeField] float _angle;
-    private float _direction = -1;
+    [SerializeField] private bool _rightDirection = true;
     [SerializeField] Transform _image;
-    private float _count = 1;
+    private float _count;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _count = _maxRotate / 2;
         StartCoroutine(Rotate(_angle));
     }
 
@@ -18,14 +20,14 @@ public class RotateImage : MonoBehaviour
     {
         while (true)
         {
-            _image.Rotate(0, 0, angle * _direction);
+            _image.Rotate(0, 0, angle * (_rightDirection ? 1: -1));
             _count--;
             if( _count == 0)
             {
-                _count = 2;
-                _direction *= -1;
+                _count = _maxRotate;
+                _rightDirection = !_rightDirection;
             }
-            yield return new WaitForSeconds(.5f);
+            yield return new WaitForSeconds(.05f);
         }
     }
 }
